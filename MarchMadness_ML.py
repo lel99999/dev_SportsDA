fcbb6357fe8797f761d060f3fd4e75c185c524ca
lel99999 <luan.le@cfpb.gov>
2022-01-08 22:41:39 -0500

    upd file/file paths

--- MarchMadness_ML.py
+++ MarchMadness_ML.py
@@ -21,7 +21,7 @@ from datetime import datetime, timedelta
 # In[2]:
 
 #file_path = '/data/basketball_college/2017_march_madness/data/'
-file_path = 'data/'
+file_path = 'data/basketball_college/2003/'
 
 reg_df = pd.read_csv(file_path + 'RegularSeasonDetailedResults.csv')
 teams_df = pd.read_csv(file_path + 'Teams.csv')
